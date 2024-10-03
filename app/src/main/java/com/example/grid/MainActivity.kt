e5a4eab2ddfe6fa09167c26c84a44369832e4e45
Dhruv Gupta <gupta8737@students.d211.org>
2024-10-03 07:35:24 -0500

    Initial commit

---
+++ app/src/main/java/com/example/grid/MainActivity.kt
@@ -0,0 +1,125 @@
+package com.example.grid
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.grid.data.DataSource
+import com.example.grid.model.Topic
+import com.example.grid.ui.theme.GridTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            GridTheme {
+                GridApp()
+            }
+        }
+    }
+}
+@Composable
+
+fun GridApp(){
+    TopicsGrid(
+        topicsList = DataSource().topics()
+    )
+}
+@Composable
+fun TopicsGrid(topicsList: List<Topic>){
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        modifier = Modifier.fillMaxSize(),
+        contentPadding = PaddingValues(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        items(topicsList){ topic ->
+            GridItem(topic)
+
+        }
+    }
+}
+
+@Composable
+fun GridItem(topic: Topic, modifier: Modifier = Modifier) {
+    Card(modifier = modifier) {
+        Row(
+            modifier = Modifier
+
+                .fillMaxWidth()
+        ) {
+            Image(
+                painter = painterResource(topic.imageResourceId),
+                contentDescription = stringResource(topic.stringResourceId),
+                modifier = Modifier
+                    .width(68.dp)
+                    .height(68.dp),
+                contentScale = ContentScale.Crop
+            )
+            Column(
+                modifier = Modifier
+                    .padding(end = 16.dp ,top = 16.dp)
+                    .fillMaxWidth()
+            ) {
+                Text(
+                    text = stringResource(topic.stringResourceId),
+                    modifier = Modifier.padding(start = 16.dp),
+                    style = MaterialTheme.typography.bodyMedium
+                )
+                Row(){
+                    Image(
+                        painter = painterResource(R.drawable.ic_grain),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .padding(start = 16.dp)
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Text(
+                        text = topic.number.toString(),
+                        modifier = Modifier.padding(top = 8.dp),
+                        style = MaterialTheme.typography.labelMedium
+                    )
+                }
+
+            }
+
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun GridPreview() {
+    GridApp()
+}
